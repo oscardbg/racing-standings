f6541b75a189dea8624ee433ade2c32477ed1b08
Vieniamin <vieniamin@mail.com>
2023-10-02 13:38:43 -0500

    Changed emojis for images and list width fixed

--- src/components/Standings.jsx
+++ src/components/Standings.jsx
@@ -1,36 +1,40 @@
 import { useGlobalContext } from "../context";
 
 const Standings = ({ num, people, locations, type }) => {
-  const { totalByRaces } = useGlobalContext();
+	const { totalByRaces } = useGlobalContext();
 
-  return (
-    <>
-      <ul className="driver-list">
-        <li className="first-row">
-          <p>Names</p>
-          <div className="scores">
-            <span>Total</span>
-            {locations.map((loc, i) => (
-              <span key={loc.id} onClick={() => totalByRaces(i + 1, type)} title={loc.race + "\n" + loc.date}>
-                {loc.flag}
-              </span>
-            ))}
-          </div>
-        </li>
-        {people.map((person) => (
-          <li key={person.id}>
-            <p>{person.name}</p>
-            <div className="scores">
-              <span>{person.total}</span>
-              {[...Array(num)].map((_, i) => (
-                <span key={i}>{person.scores[i]}</span>
-              ))}
-            </div>
-          </li>
-        ))}
-      </ul>
-    </>
-  );
+	return (
+		<>
+			<ul className="driver-list">
+				<li className="first-row">
+					<p>Names</p>
+					<div className="scores">
+						<span>Total</span>
+						{locations.map((loc, i) => (
+							<span
+								key={loc.id}
+								onClick={() => totalByRaces(i + 1, type)}
+								title={loc.race + "\n" + loc.date}
+							>
+								<img src={loc.flag} alt="" />
+							</span>
+						))}
+					</div>
+				</li>
+				{people.map((person) => (
+					<li key={person.id}>
+						<p>{person.name}</p>
+						<div className="scores">
+							<span>{person.total}</span>
+							{[...Array(num)].map((_, i) => (
+								<span key={i}>{person.scores[i]}</span>
+							))}
+						</div>
+					</li>
+				))}
+			</ul>
+		</>
+	);
 };
 
 export default Standings;
